8bb1a0b2f358c68f849227eec0a1f01bc3becbb9
MHutten <milan@DESKTOP-93V7PIS>
2023-11-06 14:52:15 +0100

    Finished Day 1 2015

--- _2015/Day_1_Not_Quite_Lisp/NotQuiteLisp.java
+++ _2015/Day_1_Not_Quite_Lisp/NotQuiteLisp.java
@@ -1,26 +1,52 @@
 package _2015.Day_1_Not_Quite_Lisp;
 
+import java.io.IOException;
+
 public class NotQuiteLisp {
     
-    public static int getSantasDestination(String directions) {
+    protected static int getSantasDestination(String directions) {
         int floor = 0;
 
         for (char direction: directions.toCharArray()) {
-            floor += followDirection(direction);
+            try {
+                floor += followDirection(direction);
+            } catch (IOException e) {
+                return 0; // TODO figure out how to test for thrown exceptions
+            }
         }
 
         return floor;
     }
 
-    private static int followDirection(char direction) {
+    private static int followDirection(char direction) throws IOException {
         switch (direction) {
             case '(':
                 return 1;
             case ')':
                 return -1;
             default:
-                // TODO throw exception
-                return 0;
+                throw new IOException("Invalid directions: contains character" + direction);
         }
     }
+
+    protected static int santaEntersBasementAt(String directions) {
+        int floor = 0;
+        int instruction_count = 0;
+
+        for (char direction: directions.toCharArray()) {
+            try {
+                floor += followDirection(direction);
+            } catch (IOException e) {
+                return 0; // TODO figure out how to test for thrown exceptions
+            }
+
+            instruction_count++;
+
+            if (floor == -1) {
+                return instruction_count;
+            }
+        }
+
+        return 0; // TODO replace with thrown Exception
+    } 
 }
--- _2015/Day_1_Not_Quite_Lisp/NotQuiteLispTest.java
+++ _2015/Day_1_Not_Quite_Lisp/NotQuiteLispTest.java
@@ -6,16 +6,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class NotQuiteLispTest {
 
     String directions;
-    int destination;
+
+    @Before
+    public void setup() {
+        directions = readInput();
+    }
 
     public String readInput() {
         try {
-            File file = new File("/input.txt");// TODO FIX, cannot find file
+            File file = new File(System.getProperty("user.dir") + "\\_2015\\Day_1_Not_Quite_Lisp\\input.txt");
             Scanner scanner = new Scanner(file);
             String output = "";
             while (scanner.hasNextLine()) {
@@ -29,6 +35,10 @@ public class NotQuiteLispTest {
             return "";
         }
     }
+
+    /* -------------------- Part 1 -------------------- */
+
+    /* ---------- Provided Examples ---------- */
 
     @Test
     public void santaEndsAtFloor0() {
@@ -55,12 +65,58 @@ public class NotQuiteLispTest {
         assertEquals(-3, NotQuiteLisp.getSantasDestination(")())())"));
     }
 
+    /* ---------- Additional Cases ---------- */
+
+    @Test
+    @Ignore
+    public void santaReceivedIncorrectDirections() {
+    }
+
+    @Test
+    @Ignore
+    public void santaWentTooHigh() {
+    }
+
+    @Test
+    @Ignore
+    public void santaWentTooLow() {
+    }
+
+    /* ---------- Problem ---------- */
+
     @Test
     public void part1() {
-        directions = readInput();
-        System.out.println(directions);
-        destination = NotQuiteLisp.getSantasDestination(directions);
+        int destination = NotQuiteLisp.getSantasDestination(directions);
         assertEquals(138, destination);
+    }
+
+    /* -------------------- Part 2 -------------------- */
+
+    /* ---------- Provided Examples ---------- */
+
+    @Test
+    public void santaEntersBasementAfterFirstDirection() {
+        assertEquals(1, NotQuiteLisp.santaEntersBasementAt(")"));
+    }
+
+    @Test
+    public void santaEntersBasementAfterFifthDirection() {
+        assertEquals(5, NotQuiteLisp.santaEntersBasementAt("()())"));
+    }
+
+    /* ---------- Additional Cases ---------- */
+
+    @Test
+    public void santaNeverEntersBasement() {
+        assertEquals(0, NotQuiteLisp.santaEntersBasementAt("")); // TODO change so it tests for thrown exception
+    }
+
+    /* ---------- Problem ---------- */
+
+    @Test
+    public void part2() {
+        int characterPosition = NotQuiteLisp.santaEntersBasementAt(directions);
+        assertEquals(1771, characterPosition);
     }
 }
 
